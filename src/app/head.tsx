dd4ef470d2fb3ba15ee9e8572834e492b32b776d
virtu <info@virtumultimedia.com>
2025-07-10 15:15:16 +0400

    asap font

---
+++ src/app/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <title>StickyLynx</title>
+      <link
+        href="https://fonts.googleapis.com/css?family=Asap:400,700&display=swap"
+        rel="stylesheet"
+      />
+    </>
+  );
+} 
